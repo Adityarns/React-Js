c466a21663c17b75bb31f8d62659c32e191f9588
Aditya Rahman Syach <adityarachmansyach@gmail.com>
2025-07-19 08:14:04 +0700

    menambahkan file props

--- 2-Dasar-dasar/Component/src/App.jsx
+++ 2-Dasar-dasar/Component/src/App.jsx
@@ -1,10 +1,13 @@
 import LoopingMenu from "./LoopingMenu";
 import MenuResto from "./MenuMakanan";
+import MenuProps from "./MenuProps";
 export default function App(){
   return(
     <>
       <MenuResto></MenuResto>
       <LoopingMenu></LoopingMenu>
+      <MenuProps hari ="Senin" mood="Bete"></MenuProps>
+      <MenuProps hari ="Selasa" mood="Happy"></MenuProps>
     </>
   )
 }
---
+++ 2-Dasar-dasar/Component/src/MenuProps.jsx
@@ -0,0 +1,7 @@
+export default function MenuProps (props){
+    return (
+        <div>
+            Hari: {props.hari} / Mood: {props.mood}
+        </div>
+    )
+}
